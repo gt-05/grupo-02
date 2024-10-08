17a120238c03557663fde04f5c961928ee686a49
unknown <BRUNO0721@DIGITALCOLLEGE.LOCAL>
2024-10-08 09:45:37 -0300

    atualizacao

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 const url = 'https://viacep.com.br/ws';
 
 const formCalcularFrete = document.getElementById('calcular-fret');
-
 const enderecoOrigem = document.getElementById('endereco-origem');
-
 const enderecoDestino = document.getElementById('endereco-destino');
 
 formCalcularFrete.addEventListener('submit', function(event){
     event.preventDefault();
 
     let cepOrigem = event.target.querySelector('#cep-origem');
+
     fetch(`${url}/${cepOrigem.value}/json`).then(function(response){
         return response.json()
     }).then(function(responseBody){
@@ -26,5 +25,36 @@ let cepDestino = event.target.querySelector('#cep-destino');
 
 });
 
+    let serviceContainer = document.querySelector('.service-container');
+
+    fetch('js/db.json').then(function(response) {
+        return response.json();
+        }).then(function(responseBody) {
+            serviceContainer.innerHTML = '';
+            for(let service of responseBody[0].services) {
+               serviceContainer.innerHTML += `
+               <li class="flex items-center bg-white m-2.5 border border-solid border-[rgba(7,255,23,0.1)] w-115">
+                        <div class="inline-block p-1.25 px-5">
+                            <p class="text-xs text-gray-500">Serviço</p>
+                            <p class="font-semibold">${service.name}</p>
+                        </div>
+                        <div class="inline-block p-1.25 px-5">
+                            <p class="text-xs text-gray-500">Prazo</p>
+                            <p class="font-semibold">${service.days}</p>
+                        </div>
+                        <div class="inline-block p-1.25 px-5">
+                            <p class="text-xs text-gray-500">Preço</p>
+                            <p class="total-price">${service.price}</p>
+                            <p class="font-weight: 600;-price">${service.priceWithDiscount}</p>
+                        </div>
+                        <div class="inline-block p-1.25 px-5">
+                            <p class="text-xs text-gray-500">Data da Postagem</p>
+                            <p class="font-semibold">19/09/2024</p>
+                        </div>
+                    </li>
+                    `;
+            }
+            console.log(responseBody);
+        })
 });
 
