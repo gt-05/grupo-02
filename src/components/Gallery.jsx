d2137e3e9fcbb19f87b894d0bf320fcce887a6e6
Bruno Morgan <fristip1997@gmail.com>
2024-10-14 12:05:01 -0300

    Gallery

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,9 @@
+export default function(Gallery) {
+[
+     { "src": "http://site.com/path/to/image1.png" }
+     { "src": "http://site.com/path/to/image2.png" }
+     { "src": "http://site.com/path/to/image3.png" }
+     { "src": "http://site.com/path/to/image4.png" }
+     { "src": "http://site.com/path/to/image5.png" }
+      ]
+    }
