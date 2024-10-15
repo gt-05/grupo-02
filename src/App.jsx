78bd95ba0c1756d008da6c42bcc7c758c00487fe
Visitante Digital <visitante.digital@DIGITALCOLLEGE.LOCAL>
2024-10-15 11:46:23 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProductListing from "./components/ProductListing.jsx"
+import Footer from "./components/Footer.jsx"
 function App() {
 
   return (
     <>
       <ProductListing />
+      <Footer />
     </>
   )
 }
