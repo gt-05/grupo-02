121ee4a038138e98ce5e3eb2524b91dbabc6cf6f
Felipe <felipe.dev.94@gmail.com>
2024-10-15 15:50:14 -0300

    Adição de alguns estilos na header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,29 @@ import shoppingIcon from "/src/assets/images/shopping-cart.svg"
 function Header (){
     return (
         <> 
-        <div className="flex space-x-4 ">
-            <Logo/>
-            <input type="text" placeholder="Pesquisar produto..." className="bg-[#F5F5F5] rounded-md p-2 w-1/4" />
-            <div className="">
-            <a href="#" className="underline ">Cadastre-se</a>
+    <div className="w-full h-48 bg-white ">
+        <div className="px-20 py-10">
+            <div className="flex space-x-6 ">
+                <Logo/>
+                <input type="text" placeholder="Pesquisar produto..." className="bg-[#F5F5F5] rounded-md p-2 w-5/12" />
+                <div className="flex items-center pl-6 ">
+                <a href="#" className="underline ">Cadastre-se</a>
+                </div>
+                <button className="w-28 h-10 text-[#F5F5F5] bg-[#C92071] rounded-md" >Entrar</button>
+                <div className="pl-10">
+                <img src= {shoppingIcon} alt="Icone de carrinho de compras" />  
+                </div>
             </div>
-            <button className="w-28 h-10 text-[#F5F5F5] bg-[#C92071] rounded-md" >Entrar</button>
-            <img src= {shoppingIcon} alt="Icone de carrinho de compras" />  
+        <   div className="pl-2 pt-14">
+                <nav className=" flex space-x-6">
+                    <a className="font-bold underline text-[#C92071]" href="#">Home</a>
+                    <a href="#">Produtos</a>
+                    <a href="#">Categorias</a>
+                    <a href="#">Meus Pedidos</a>
+                </nav>
+            </div>
         </div>
-        <div >
-            <nav className="flex space-x-4">
-                <a className="font-bold underline text-[#C92071]" href="#">Home</a>
-                <a href="#">Produtos</a>
-                <a href="#">Categorias</a>
-                <a href="#">Meus Pedidos</a>
-            </nav>
-        </div>
+    </div>
         </>
     )
 }
