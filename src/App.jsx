2491f4947a55a156b86277bc8401e518da1db2a0
unknown <BRUNO0721@DIGITALCOLLEGE.LOCAL>
2024-10-15 11:25:36 -0300

    atualizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Gallery from "./components/Gallery.jsx"
 import ProductListing from "./components/ProductListing.jsx"
+
 function App() {
 
   return (
     <>
-      <ProductListing />
+      <Gallery/>
+     
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
