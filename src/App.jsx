d4ea841865f5734a91cb2ee26eebb5e67310a5cb
unknown <BRUNO0721@DIGITALCOLLEGE.LOCAL>
2024-10-16 10:39:56 -0300

    Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Gallery from "./components/Gallery.jsx"
 import ProductListing from "./components/ProductListing.jsx"
 import Header from './components/Header.jsx'
+import Section from "./components/section.jsx";
 
 function App() {
 
@@ -10,8 +11,18 @@ function App() {
       <Header/>
     
       <Gallery/>
-     
+
+      <Section
+      title="Produtos em Alta"
+      titleAlign="left"
+      link={
+        {
+          "text": "ver todos",
+          "href": "https://redirect.link"
+        }
+      }>
       <ProductListing />
+      </Section>
     </>
   );
 }
