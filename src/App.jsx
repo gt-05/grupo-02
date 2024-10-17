b0d25c87cf146c36c5eb594a6b2e41d1a0474bad
unknown <BRUNO0721@DIGITALCOLLEGE.LOCAL>
2024-10-17 10:40:30 -0300

    colocando routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-import { useState } from "react";
-function App() {
-const[page, setPage] = useState("Home");
-  return (
-    <>
-    <div>
-      <a href="#" onClick={event => setPage('Home')}>pagina1</a>
-    </div>
-    <div>
-    <a href="#" onClick={event => setPage('ProductListingPage')}>pagina2</a>
-    </div>
-    <div>
-    <a href="#" onClick={event => setPage('ProductViewPage')}>pagina3</a>
-    </div>
-    <hr />
-    <div>
-      {page}
-    </div>
-    </>
-  );
+import {
+  RouterProvider
+} from 'react-router-dom';
+import routes from './routes';
+
+function App(){
+   return <RouterProvider router={routes}/>
 }
 
 export default App;
