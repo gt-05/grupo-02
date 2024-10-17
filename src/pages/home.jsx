80c42e2a4f86d0ead071789f3bb83b64ba07a8c7
unknown <BRUNO0721@DIGITALCOLLEGE.LOCAL>
2024-10-17 09:02:20 -0300

    atualizar

---
+++ src/pages/home.jsx
@@ -0,0 +1,29 @@
+import ProductListing from "../ProductListing.jsx"
+import Gallery from "../Gallery.jsx"
+import Header from '../Header.jsx'
+import Section from "../section.jsx";
+import Footer from "../Footer.jsx"
+import Group from "../assets/images/Group"
+import Layout from "../Layout.jsx"
+
+export default function Home() {
+    return (
+        <>
+        <Header/>
+        <Gallery/>
+        <img src={Group 53592}/>
+        <Section
+        title="Produtos em Alta"
+        titleAlign="left"
+        link={
+            {
+            "text": "ver todos",
+            "href": "https://redirect.link"
+            }
+        }>
+        <ProductListing />
+        </Section>
+        <Footer/>
+    </>
+    )
+}
