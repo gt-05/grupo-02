9453cd1c8839fa3584c7d3c75b76b3a46eacb046
unknown <BRUNO0721@DIGITALCOLLEGE.LOCAL>
2024-10-21 09:13:57 -0300

    atualizar Section

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -1,12 +1,22 @@
-function Section(props) {
-    return (
-        <div className="i">
-            <div className="header">
-                <span className="beginning">Produtos em alta</span>
-                <span className="ending">Ver todos<img className="arrow" src='src/assets/images/arrow.svg' /></span>
+export default function Section(props) {
+    
+    let align = props.titleAlign ? props.titleAlign: 'left';
+    let position = `text-${align}`;
+    return(
+        <>
+        <div className="flex">
+        <div className={position + ' grow'}>
+            <h2>{props.title}</h2>
             </div>
-                {props.children}
-        </div>
-    )
+           {props.link && (
+               <div className="w-auto">
+           <a href={props.link.href}>{props.link.text}</a>
+       </div>
+       )}
+       </div>
+       
+       {props.children}
+       </>
+   )
+   
 }
-export default Section
