a702a79b8c35f814b4fc4713c9a0c91e522b6e63
unknown <BRUNO0721@DIGITALCOLLEGE.LOCAL>
2024-11-04 14:00:37 -0300

    add o codigo antigo no comentario de prodctlister

--- frontend/src/components/ProductListing.jsx
+++ frontend/src/components/ProductListing.jsx
@@ -25,7 +25,7 @@ function ProductListing({ maxNumber }) {
     return (
         <div className="flex flex-wrap m-auto gap-[22px] justify-center">
             {products.slice(0, maxNumber).map((product, index) => {
-                let image = product.images[0]?.url ?? product.placeHolder_image; // Usando let aqui
+                let image = product.images[0]?.url ?? product.placeHolder_image;
 
                 return (
                     <ProductCard
@@ -43,3 +43,37 @@ function ProductListing({ maxNumber }) {
 }
 
 export default ProductListing;
+
+
+/**
+ import { useEffect, useState } from "react";
+import ProductCard from "./ProductCard.jsx"
+
+function ProductListing(props){
+    const url = 'https://raw.githubusercontent.com/gt-05/.github/refs/heads/main/db.json'
+    let [products, setProducts] = useState([]);
+
+    useEffect(function (){
+        fetch(url).then(function(response) {return response.json()}).then(function(body) {
+            setProducts(body.products)
+        })
+    },[])
+
+    return(
+    <>
+        <div className="flex flex-wrap m-auto gap-[22px] justify-center">
+            {products.slice(0, props.maxNumber).map((product, index) =>
+                <ProductCard
+                key={index}
+                image={product.images[0]}
+                name={product.name}
+                price={product.price}
+                priceDiscount={product.price_with_discount}
+                />
+            )}
+        </div>
+    </> 
+    )
+}
+export default ProductListing
+ */
